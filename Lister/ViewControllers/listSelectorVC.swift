c0b8f9dd5e76beffe0fc8613142673e5ae310f47
CalDevC <mr.alexander5132@gmail.com>
2022-01-06 20:03:22 -0800

    Added files for VCs

---
+++ Lister/ViewControllers/listSelectorVC.swift
@@ -0,0 +1,17 @@
+//
+//  listSelectorVC.swift
+//  Lister
+//
+//  Created by Chase Alexander on 1/4/22.
+//
+
+import UIKit
+
+class listSelectorVC: UICollectionViewController{
+  
+  override func viewDidLoad() {
+    super.viewDidLoad();
+    
+  }
+  
+}
---
+++ Lister/ViewControllers/listVC.swift
@@ -0,0 +1,16 @@
+//
+//  listVC.swift
+//  Lister
+//
+//  Created by Chase Alexander on 1/4/22.
+//
+
+import UIKit
+
+class listVC: UIViewController{
+  
+  override func viewDidLoad() {
+    super.viewDidLoad();
+  }
+  
+}
